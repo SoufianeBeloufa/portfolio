93505da57d7615b9c4794b470c60b7e39f0223f6
unknown <s.beloufa@esi-sba.dz>
2023-12-25 01:23:11 +1300

    no backend

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  publicPath:'/portfolio.github.io/'
 })
